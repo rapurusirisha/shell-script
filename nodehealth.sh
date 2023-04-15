4884d093cde47e8bc812e1924557a391e80434fe
sirisha <rapurusirisha123@gmail.com>
2023-04-15 13:13:46 +0000

    first node health check status

---
+++ nodehealth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+######
+#Author: sirisha
+#date: 27/3/2023
+#this script outputs the node health
+#version :v1
+#######
+
+set -x
+set -e
+set -o pipefail
+
+#echo " print the disk space "
+
+df -h
+
+#echo " print the cpu "
+
+nproc
+
+#echo " print the RAM "
+
+free -g
+
+ps -ef | grep amazon | awk -F" " '{print $2}'
+
