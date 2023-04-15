c62a5fbe175463404e8212e41c42374c27e3c7c7
sirisha <rapurusirisha123@gmail.com>
2023-04-15 13:56:14 +0000

    prime no script first

---
+++ primno.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Prime numbers between 1 and 100 are:"
+
+for ((i=2;i<=100;i++))
+do
+  flag=0
+  for ((j=2;j<=i/2;j++))
+  do
+    if [ $((i%j)) -eq 0 ]
+    then
+      flag=1
+      break
+    fi
+  done
+  if [ $flag -eq 0 ]
+  then
+    echo $i
+  fi
+done
+
+
