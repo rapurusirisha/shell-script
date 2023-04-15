c3809680002aaf06ddc73181326e4b1371cb86ff
sirisha <rapurusirisha123@gmail.com>
2023-04-15 13:04:38 +0000

    second commit

--- aws-resource-tracker.sh
+++ aws-resource-tracker.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #Athor:sirisha
 #date:15/04/2023
-#Version:V1
+#Versiion:V1
 #to list the S3 Buckets
 set -x
 echo " print s3 buckets "
 aws s3 ls
 # to list the ec2-instances
-echo " print ec2 buckets "
+echo " print ec2 Instances "
 aws ec2 describe-instances
 # to list the lambda functions
-echo " print lambda functions uckets "
+echo " print lambda functions "
 aws lambda list-functions
 # to list iam-users
-echo " print iam-users buckets "
+echo " print iam-users "
 aws iam list-users
-
 
